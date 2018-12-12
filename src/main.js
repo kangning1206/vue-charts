360ca7546d858be2dc708be08fee8b3169f30c6f
kangning1206 <kangning1206@163.com>
2018-12-12 11:15:44 +0800

    up

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 // 在 ./App.vue 前引用，避免加载顺序
 import ElementUI from 'element-ui';
 
+// 类似reset.css，修正和重置浏览器样式
 import 'normalize.css';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -11,8 +12,9 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
-import '@/assets/css/flex.less';
+// import '@/assets/css/flex.less';
 import '@/assets/css/base.css';
+// 引入自定义的iconfont扩展element icon
 import '@/assets/css/iconfont/iconfont.css';
 
 Vue.config.productionTip = false;
