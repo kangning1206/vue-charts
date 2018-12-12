7514bad9a7303637e00922da6a1e7a11fadf8d38
kangning1206 <kangning1206@163.com>
2018-12-12 12:36:39 +0800

    up

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@
  * @Author: kangning1206
  * @Date:   2018-12-12 11:20:38
  * @Last Modified by:   kangning1206
- * @Last Modified time: 2018-12-12 11:28:14
+ * @Last Modified time: 2018-12-12 12:35:15
  */
 
 // vue.config.js
 module.exports = {
-  baseUrl: '/vue-charts/',
-  outputDir: 'docs',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/vue-charts/' : '/',
+  outputDir: process.env.NODE_ENV === 'production' ? 'docs' : 'dist',
 };
